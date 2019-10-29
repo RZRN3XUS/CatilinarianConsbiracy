5a69a2cbadc74fe8c9b077dc26c6116449af27a0
RZRN3XUS <dycusc22@greenhill.org>
2019-10-29 09:56:04 -0500

    fuck

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,9 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    System.out.println("\u001B321;1mCatilinarian Conspiracy Simulator: Fate of the Conspirators");
     }
 }
