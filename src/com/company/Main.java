d525e2abe40f8cb58db70a8d23e959d00997a9fd
RZRN3XUS <dycusc22@greenhill.org>
2019-10-30 09:57:19 -0500

    fuck

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,10 +18,26 @@ public class Main {
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
-	    System.out.println(ANSI_GREEN + "Catilinarian Conspiracy Simulator: Fate of the Conspirators");
-	    System.out.println(ANSI_CYAN + "Created by Christopher and Xander\n");
-	    System.out.println();
+        System.out.println(ANSI_GREEN + "Catilinarian Conspiracy Simulator: Fate of the Conspirators");
+        System.out.println(ANSI_CYAN + "Created by Christopher and Xander\n");
+        System.out.println(ANSI_YELLOW + "You are the Senate in ancient Rome.");
+        System.out.println(ANSI_YELLOW + "Your objective is to choose which speaker's side to agree with.");
+        System.out.println(ANSI_YELLOW + "The fate of Rome is in your hands.\n");
+        System.out.println("\033[3mType any key and enter to continue\033[0m");
+        String input = sc.next();
+        System.out.print("Loading.");
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(500);
+            System.out.print(".");
+        }
+        System.out.println();
+        System.out.println(ANSI_PURPLE + "The day is December 5, 62 BC. Cicero has called the senate to the Temple of Concord to decide the fate of the 5 captured conspirators.");
+        Thread.sleep(2000);
+        System.out.println(ANSI_BLACK + "       Cicero proposes that all conspirators in custody be executed, along with the four currently being sought.");
+        System.out.println(ANSI_BLACK + "       Julius Caesar: I do not fear that M. Tullius [would do] these things or that [they will happen] at this time; but in a great state there are many and varied characters. Perhaps at another time, when another consul had command ofthe army, a false [accusation] will be believed as true. If some future consul, using our ded as an example,\r       draws his sword in accordance with the Senate’s decree, who will impose a limit or restrain him?\n");
     }
 }
+
